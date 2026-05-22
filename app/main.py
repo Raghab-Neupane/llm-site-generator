f9d57da0c4b96e8c16b8c905467230ca80ee8ea8
RAGHAB NEUPANE <neupaneraghab@gmail.com>
2026-05-22 11:40:27 +0545

    changes

--- app/main.py
+++ app/main.py
@@ -3,9 +3,13 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+
 from app.api.routes import router
+from app.api.authentication.auth_routes import router as auth_router
+
 
 app = FastAPI()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,11 +19,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+# Main API routes
 app.include_router(router)
+
+
+# Authentication routes
+app.include_router(
+    auth_router,
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
 
 FRONTEND_PATH = (
     Path(__file__).resolve().parent.parent
-    / "frontend" / "index.html"
+    / "frontend"
+    / "index.html"
 )
 
 
